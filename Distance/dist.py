e0c85f4427c9fd39e84da881b5c43ed6fcca2f8a
junghun9806 <46277723+junghun9806@users.noreply.github.com>
2022-02-17 14:41:33 +0900

    NewFile

---
+++ Distance/dist.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Last update: 2021-08-11
+Moduel information
+
+Required module. 
+    numpy
+
+Code written by Junghun Chae. 
+Contact: junghun98@unist.ac.kr 
+wjdgnswkd612@gmail.com
+"""
+
+"""
+Develop Note. 
+Things to do. 
+"""
